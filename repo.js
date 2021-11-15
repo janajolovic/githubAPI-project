35dd01fc66cf7f772082f8db7f7ef295cc246154
Jana Jolovic <janajolovic04@gmail.com>
2021-11-15 19:28:55 +0100

    repositories card update

--- repo.js
+++ repo.js
@@ -5,19 +5,23 @@ const createCard = () => {
     fetch("https://api.github.com/users/janajolovic/repos")
         .then(response => response.json())
         .then(data => {
-                    let html = `<a href="index.html"class='close-btn'><i class="fas fa-times"></i></a>`;
+            let html = "";
             data.forEach(repo => {
                 html += `
                     <div class="repo">
+                        <a href="index.html" class="close-btn"><i class="fas fa-times"></i></a>
                         <div class="repo-data">
                             <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                             <div id="visibility">${repo.visibility}</div>
                         </div>
-                        <p>${repo.language}</p>
-                `
-
-                html += `<p class="created">created at ${(repo.created_at).slice(0,10)}</p>
-                        </div>`
+                        `
+                        if (repo.description) {
+                            html += `<p class="desc">${repo.description}</p>`
+                        }
+                        
+                        html += `<p>${repo.language}</p>
+                                <p class="created">updated at ${(repo.updated_at).slice(0,10)}</p>
+                            </div>`
             })
             card.innerHTML = html
         })
