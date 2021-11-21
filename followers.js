d1bc9e46a5a2cf576f518b67713bfe75bb22b1d8
Jana Jolovic <janajolovic04@gmail.com>
2021-11-21 23:24:14 +0100

    async func

--- followers.js
+++ followers.js
@@ -1,26 +1,24 @@
 const card = document.querySelector(".card");
 
 
-const createCard = () => {
-    fetch("https://api.github.com/users/janajolovic/followers")
-        .then(response => response.json())
-        .then(data => {
-            let html = "";
-            data.forEach(user => {
-                html += `
-                    <div class="user">
-                        <div class="img">
-                            <img src="${user.avatar_url}" alt="">
-                        </div>
-                        <div class="user-data">
-                            <a href="${user.html_url}" target="_blank">${user.login}</a>
-                        </div>
-                    </div>
-                `
-            })
-            html += `<a href="index.html" class="close-btn"><i class="fas fa-times"></i></a>`
-            card.innerHTML = html
-        })
+const createCard = async () => {
+    response = await fetch("https://api.github.com/users/janajolovic/followers")
+    data = await response.json();
+    let html = "";
+    data.forEach(user => {
+        html += `
+            <div class="user">
+                <div class="img">
+                    <img src="${user.avatar_url}" alt="">
+                </div>
+                <div class="user-data">
+                    <a href="${user.html_url}" target="_blank">${user.login}</a>
+                </div>
+            </div>
+        `
+    })
+    html += `<a href="index.html" class="close-btn"><i class="fas fa-times"></i></a>`
+    card.innerHTML = html
 }
 
 
--- repo.js
+++ repo.js
@@ -1,31 +1,29 @@
 const card = document.querySelector(".card");
 
 
-const createCard = () => {
-    fetch("https://api.github.com/users/janajolovic/repos")
-        .then(response => response.json())
-        .then(data => {
-            let html = "";
-            data.forEach(repo => {
-                html += `
-                    <div class="repo">
-                        
-                        <div class="repo-data">
-                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                            <div id="visibility">${repo.visibility}</div>
-                        </div>
-                        `
-                        if (repo.description) {
-                            html += `<p class="desc">${repo.description}</p>`
-                        }
-                        
-                        html += `<p>${repo.language}</p>
-                                <p class="created">updated at ${(repo.updated_at).slice(0,10)}</p>
-                            </div>`
-            })
-            html += `<a href="index.html" class="close-btn"><i class="fas fa-times"></i></a>`
-            card.innerHTML = html
-        })
+const createCard = async () => {
+    response = await fetch("https://api.github.com/users/janajolovic/repos")
+    data = await response.json();
+    let html = "";
+    data.forEach(repo => {
+        html += `
+            <div class="repo">
+                
+                <div class="repo-data">
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    <div id="visibility">${repo.visibility}</div>
+                </div>
+                `
+                if (repo.description) {
+                    html += `<p class="desc">${repo.description}</p>`
+                }
+                
+                html += `<p>${repo.language}</p>
+                        <p class="created">updated at ${(repo.updated_at).slice(0,10)}</p>
+                    </div>`
+    })
+    html += `<a href="index.html" class="close-btn"><i class="fas fa-times"></i></a>`
+    card.innerHTML = html;
 }
 
 
