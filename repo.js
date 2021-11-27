1c7d7b2644510cde0400cb1e13c6952d46a332af
Jana Jolovic <janajolovic04@gmail.com>
2021-11-27 20:56:40 +0100

    changes

--- repo.js
+++ repo.js
@@ -4,26 +4,53 @@ const card = document.querySelector(".card");
 const createCard = async () => {
     response = await fetch("https://api.github.com/users/janajolovic/repos")
     data = await response.json();
-    let html = "";
+
     data.forEach(repo => {
-        html += `
-            <div class="repo">
                 
-                <div class="repo-data">
-                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                    <div id="visibility">${repo.visibility}</div>
-                </div>
-                `
-                if (repo.description) {
-                    html += `<p class="desc">${repo.description}</p>`
-                }
-                
-                html += `<p>${repo.language}</p>
-                        <p class="created">updated at ${(repo.updated_at).slice(0,10)}</p>
-                    </div>`
+        const repo_div = document.createElement("div");
+        repo_div.classList.add("repo");
+
+        const repo_data = document.createElement("div");
+        repo_data.classList.add("repo-data")
+        const name = document.createElement("a");
+        name.href = repo.html_url;
+        name.target = "_blank";
+        name.innerHTML = repo.name;
+        repo_data.appendChild(name);
+        const vis = document.createElement("div");
+        vis.id = "visibility";
+        vis.innerHTML = repo.visibility;
+        repo_data.appendChild(vis);
+
+        repo_div.appendChild(repo_data);
+
+        if (repo.description) {
+            const desc = document.createElement("p");
+            desc.classList.add("desc");
+            desc.innerHTML = repo.description;
+            repo_div.appendChild(desc);
+        }
+            
+        const lang = document.createElement("p");
+        lang.innerHTML = repo.language;
+        repo_div.appendChild(lang);
+
+        const updated = document.createElement("p");
+        updated.classList.add("created");
+        updated.innerHTML = `updated at ${(repo.updated_at).slice(0,10)}`;
+        repo_div.appendChild(updated);
+
+        card.appendChild(repo_div);
+
     })
-    html += `<a href="index.html" class="close-btn"><i class="fas fa-times"></i></a>`
-    card.innerHTML = html;
+
+    const btn = document.createElement("a");
+    btn.href = "index.html";
+    btn.classList.add("close-btn");
+    btn.innerHTML = `<i class="fas fa-times"></i>`;
+
+    card.append(btn); 
+
 }
 
 
